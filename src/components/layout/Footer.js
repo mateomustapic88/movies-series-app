a6b743781828c10c2c6703dd05f22e1b289dfe27
mateo.mustapic <mateo.mustapic88@gmail.com>
2020-09-30 11:34:46 +0200

    initial commit with Navbar and Footer components

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="footer p-3 mt-4 text-center bg-dark text-light">
+            Developed By:&nbsp;	
+            <span className="text-warning font-weight-normal">
+              Mateo Mustapic
+            </span>
+            , using <i className="fab fa-react" /> React JS &amp; Redux JS
+            integrated with external movies data API
+            <a
+              href="https://www.themoviedb.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              &nbsp;TMDB
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
