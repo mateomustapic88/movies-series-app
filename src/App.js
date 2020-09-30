e1365ce42317a6d2a16783b934e3ee09b5999a52
mateo.mustapic <mateo.mustapic88@gmail.com>
2020-09-30 15:12:50 +0200

    format all files with Prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 class App extends Component {
-  render () {
-    return (
-      <div>
-        <Navbar />
-        <Footer />
-      </div>
-    ) ;
-  } 
+	render() {
+		return (
+			<div>
+				<Navbar />
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
