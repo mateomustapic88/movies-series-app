b4f29617e7ca75b567c69915ec7f30ca7b3c335f
mateo.mustapic <mateo.mustapic88@gmail.com>
2020-10-02 10:18:54 +0200

    Updates

---
+++ precache-manifest.55cf4df6a6a85c86dbeacc355ee30739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ffc70eddd160878b7a9ccd7152ed99",
+    "url": "/%7Bmovies-series-app%7D/index.html"
+  },
+  {
+    "revision": "b02c98382f78ad03fcdf",
+    "url": "/%7Bmovies-series-app%7D/static/css/main.114380fd.chunk.css"
+  },
+  {
+    "revision": "c96611169518ab86b3ee",
+    "url": "/%7Bmovies-series-app%7D/static/js/2.1254c6b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%7Bmovies-series-app%7D/static/js/2.1254c6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02c98382f78ad03fcdf",
+    "url": "/%7Bmovies-series-app%7D/static/js/main.13db147a.chunk.js"
+  },
+  {
+    "revision": "f4f23cb2743a7c0a2855",
+    "url": "/%7Bmovies-series-app%7D/static/js/runtime-main.22e04112.js"
+  }
+]);
