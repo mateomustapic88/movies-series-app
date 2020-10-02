0d02916d1a194ee260763a540619bd5b4dc34f07
mateo.mustapic <mateo.mustapic88@gmail.com>
2020-10-02 09:57:38 +0200

    fix footer position at the bottom

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 function Footer() {
 	return (
-		<div>
-			<div className="row">
-				<div className="col-md-12">
-					<div className="footer p-3 mt-4 text-center bg-dark text-light">
+		<div className='app-footer'>
+			<div className='row'>
+				<div className='col-md-12'>
+					<div className='footer p-3 mt-4 text-center bg-dark text-light'>
 						Developed By:&nbsp;
-						<span className="text-warning font-weight-normal">
+						<span className='text-warning font-weight-normal'>
 							Mateo Mustapic
 						</span>
-						, using <i className="fab fa-react" /> React JS &amp; Redux JS
+						, using <i className='fab fa-react' /> React JS &amp; Redux JS
 						integrated with external movies data API
 						<a
-							href="https://www.themoviedb.org/"
-							target="_blank"
-							rel="noopener noreferrer"
+							href='https://www.themoviedb.org/'
+							target='_blank'
+							rel='noopener noreferrer'
 						>
 							&nbsp;TMDB
 						</a>
